c484d3b08c84f40d1d077e32ea9925619d6ec69c
Kahan-Chokshi <kahan.chokshi@kcsitglobal.com>
2022-04-15 13:33:26 +0530

    Input/Output

---
+++ Angular/Input-Output/src/app/gamecontrol/gamecontrol.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-gamecontrol',
+  templateUrl: './gamecontrol.component.html',
+  styleUrls: ['./gamecontrol.component.css']
+})
+export class GamecontrolComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onStartNumber() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
+
+  onStopNumber() {
+    clearInterval(this.interval);
+  }
+
+}
