40f817026e015643cb9cbf2975ff4adcc3f4ba00
Kahan-Chokshi <kahan.chokshi@kcsitglobal.com>
2022-05-11 11:42:29 +0530

    11/05/22

--- Ticketing Web Application/react/src/UI/Buttons/NormalButtons/NormalGradientButton/NormalGradientButton.tsx
+++ Ticketing Web Application/react/src/UI/Buttons/NormalButtons/NormalGradientButton/NormalGradientButton.tsx
@@ -1,64 +1,102 @@
 import React from "react";
-import classes from './NormalGradientButton.module.css'
+import classes from "./NormalGradientButton.module.css";
 
-const NormalGradientButton:React.FC<{
-    text:string,buttonColor:string,
-    cssClassesOnContainer?:string[],
-    cssClassesOnButton?:string[],
-    type?:string,
-    disabled?:boolean,
-    onClick?: () => void } > = props => {
-    let colorClass:string;
-    switch (props.buttonColor){
-        case 'green':
-            colorClass = classes.green;
-            break
-        case 'red':
-            colorClass = classes.red;
-            break;
-        case 'violet':
-            colorClass = classes.violet;
-            break;
-        case 'pink':
-            colorClass = classes.pink;
-            break;
-        default:
-            colorClass = "";
-            break;
-    }
-    let convertedButtonClasses = ' ';
-    props.cssClassesOnButton?.forEach((className) =>{
-        convertedButtonClasses += className+' '
-    });
-    let topButton;
-    switch (props.type) {
-        case 'submit':
-            topButton = <button type={"submit"} disabled={props.disabled} onClick={props.onClick}
-                                className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}>{props.text}</button>
-            break;
-        case 'reset':
-            topButton =<button type={"reset"} disabled={props.disabled} onClick={props.onClick}
-                               className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}>{props.text}</button>
-            break;
-        case 'button':
-            topButton =<button type={"button"} disabled={props.disabled} onClick={props.onClick}
-                               className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}>{props.text}</button>
-            break;
-        default:
-            topButton =<button disabled={props.disabled} onClick={props.onClick}
-                               className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}>{props.text}</button>
-            break
-    }
-    let convertedContainerClasses = ' '
-    props.cssClassesOnContainer?.forEach((className) =>{
-        convertedContainerClasses += className+' '
-    });
-    return (
-        <div className={`${classes.container} ${convertedContainerClasses}`} >
-            {topButton}
-            <button className={`${classes.background} ${colorClass} ${classes.blur}`} onClick={props.onClick}
-                    disabled={props.disabled} >{props.text}</button>
-        </div>
-    );
-}
-export default NormalGradientButton
+const NormalGradientButton: React.FC<{
+  text: string;
+  buttonColor: string;
+  cssClassesOnContainer?: string[];
+  cssClassesOnButton?: string[];
+  type?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}> = (props) => {
+  let colorClass: string;
+  switch (props.buttonColor) {
+    case "green":
+      colorClass = classes.green;
+      break;
+    case "red":
+      colorClass = classes.red;
+      break;
+    case "violet":
+      colorClass = classes.violet;
+      break;
+    case "pink":
+      colorClass = classes.pink;
+      break;
+    default:
+      colorClass = "";
+      break;
+  }
+  let convertedButtonClasses = " ";
+  props.cssClassesOnButton?.forEach((className) => {
+    convertedButtonClasses += className + " ";
+  });
+  let topButton;
+  switch (props.type) {
+    case "submit":
+      topButton = (
+        <button
+          type={"submit"}
+          disabled={props.disabled}
+          onClick={props.onClick}
+          className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}
+        >
+          {props.text}
+        </button>
+      );
+      break;
+    case "reset":
+      topButton = (
+        <button
+          type={"reset"}
+          disabled={props.disabled}
+          onClick={props.onClick}
+          className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}
+        >
+          {props.text}
+        </button>
+      );
+      break;
+    case "button":
+      topButton = (
+        <button
+          type={"button"}
+          disabled={props.disabled}
+          onClick={props.onClick}
+          className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}
+        >
+          {props.text}
+        </button>
+      );
+      break;
+    default:
+      topButton = (
+        <button
+          disabled={props.disabled}
+          onClick={props.onClick}
+          className={`${classes.btn} ${colorClass} ${convertedButtonClasses}`}
+        >
+          {props.text}
+        </button>
+      );
+      break;
+  }
+  let convertedContainerClasses = " ";
+  props.cssClassesOnContainer?.forEach((className) => {
+    convertedContainerClasses += className + " ";
+  });
+  return (
+    <div className={`${classes.container} ${convertedContainerClasses}`}>
+      {topButton}
+      <button
+        className={`${classes.background} ${colorClass} ${classes.blur}`}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.text}
+      </button>
+    </div>
+  );
+};
+export default NormalGradientButton;
--- Ticketing Web Application/react/src/UI/GradientInput/GradientInput.tsx
+++ Ticketing Web Application/react/src/UI/GradientInput/GradientInput.tsx
@@ -1,49 +1,64 @@
-import React, { useState} from "react";
-import classes from './GradientInput.module.css'
-const GradientInput = React.forwardRef<HTMLInputElement,{type:string,
-    color:string,
-    placeHolder:string,
-    label:string,
-    value?:any,
-    cssClasses?: string[],
-    onBlurHandler?:(e:React.FocusEvent<HTMLInputElement>) => void,
-    onChangeHandler?: (e:React.ChangeEvent<HTMLInputElement>) => void }>((props,
-                                                                          ref) => {
-        const [InputClasses,setInputClasses] = useState(`${classes.background}`);
-        let colorClass:string;
-        switch (props.color){
-            case 'green':
-                colorClass = classes.green;
-                break
-            case 'red':
-                colorClass = classes.red;
-                break;
-            case 'violet':
-                colorClass = classes.violet;
-                break;
-            default:
-                colorClass = "";
-                break;
-        }
-        const onFocusHandler: () => void = () => {
-            setInputClasses(`${classes.background} ${colorClass}`)
-        }
-        const onBlurHandler: (e:React.FocusEvent<HTMLInputElement>) => void = (e) => {
-            setInputClasses(`${classes.background}`);
-            const x:React.FocusEventHandler<HTMLInputElement> | undefined = props.onBlurHandler;
-            if (x) {
-                x(e);
-            }
-        }
-        return (
-            <div className={`${classes.inputContainer} ${props.cssClasses}`}>
-                <label className={classes.labelText}>{props.label}</label>
-                <div className={classes.inputSubContainer}>
-                    <input value={props.value} ref={ref} onFocus={onFocusHandler} onBlur={onBlurHandler} onChange={props.onChangeHandler}
-                           className={classes.normal} type={props.type} placeholder={props.placeHolder}/>
-                    <div className={InputClasses}/>
-                </div>
-            </div>
-        );
+import React, { useState } from "react";
+import classes from "./GradientInput.module.css";
+const GradientInput = React.forwardRef<
+  HTMLInputElement,
+  {
+    type: string;
+    color: string;
+    placeHolder: string;
+    label: string;
+    value?: any;
+    cssClasses?: string[];
+    onBlurHandler?: (e: React.FocusEvent<HTMLInputElement>) => void;
+    onChangeHandler?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  }
+>((props, ref) => {
+  const [InputClasses, setInputClasses] = useState(`${classes.background}`);
+  let colorClass: string;
+  switch (props.color) {
+    case "green":
+      colorClass = classes.green;
+      break;
+    case "red":
+      colorClass = classes.red;
+      break;
+    case "violet":
+      colorClass = classes.violet;
+      break;
+    default:
+      colorClass = "";
+      break;
+  }
+  const onFocusHandler: () => void = () => {
+    setInputClasses(`${classes.background} ${colorClass}`);
+  };
+  const onBlurHandler: (e: React.FocusEvent<HTMLInputElement>) => void = (
+    e
+  ) => {
+    setInputClasses(`${classes.background}`);
+    const x: React.FocusEventHandler<HTMLInputElement> | undefined =
+      props.onBlurHandler;
+    if (x) {
+      x(e);
+    }
+  };
+  return (
+    <div className={`${classes.inputContainer} ${props.cssClasses}`}>
+      <label className={classes.labelText}>{props.label}</label>
+      <div className={classes.inputSubContainer}>
+        <input
+          value={props.value}
+          ref={ref}
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+          onChange={props.onChangeHandler}
+          className={classes.normal}
+          type={props.type}
+          placeholder={props.placeHolder}
+        />
+        <div className={InputClasses} />
+      </div>
+    </div>
+  );
 });
-export default GradientInput
+export default GradientInput;
