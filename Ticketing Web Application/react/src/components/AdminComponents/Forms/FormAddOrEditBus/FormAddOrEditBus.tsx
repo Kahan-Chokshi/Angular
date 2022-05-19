57b09598a1e8186315f284c3e2cf15366e1c0f2e
Kahan-Chokshi <kahan.chokshi@kcsitglobal.com>
2022-05-19 11:23:06 +0530

    19/05/22

--- Ticketing Web Application/react/src/components/AdminComponents/Forms/FormAddOrEditBus/FormAddOrEditBus.tsx
+++ Ticketing Web Application/react/src/components/AdminComponents/Forms/FormAddOrEditBus/FormAddOrEditBus.tsx
@@ -1,158 +1,195 @@
-import React, {useEffect, useRef, useState} from "react";
-import classes from './FormAddOrEditBus.module.css';
-import close from '../../../../images/close.svg'
+import React, { useEffect, useRef, useState } from "react";
+import classes from "./FormAddOrEditBus.module.css";
+import close from "../../../../images/close.svg";
 import GrayCard from "../../../../UI/GrayCard/GrayCard";
-import NormalGradientImageButton
-    from "../../../../UI/Buttons/NormalButtons/NormalGradientImageButton/NormalGradientImageButton";
+import NormalGradientImageButton from "../../../../UI/Buttons/NormalButtons/NormalGradientImageButton/NormalGradientImageButton";
 import GradientInput from "../../../../UI/GradientInput/GradientInput";
 import NormalGradientButton from "../../../../UI/Buttons/NormalButtons/NormalGradientButton/NormalGradientButton";
 import fetch from "node-fetch";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../../store";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../store";
 
 export type stop = {
-    name: string,
-    _id: string
-}
+  name: string;
+  _id: string;
+};
 const FormAddOrEditBus: React.FC<{
-    mode: 'Add' | 'Edit',
-    onCloseHandler: () => void,
-}> = props => {
-    const selectedBusId = useSelector((state: RootState) => state.busesList.selectedBusId);
-    const [busNumber, setBusNumber] = useState<string>('')
-    const [stops, setStops] = useState<stop[]>([])
-    const busNumberInputRef = useRef(document.createElement('input'))
-    const {mode} = props;
-    useEffect(() => {
-        if (mode === 'Edit' && selectedBusId) {
-            //send request to fetch Data
-            console.log(selectedBusId)
-            console.log(`sending request with busId ${selectedBusId}`);
-            fetch(`http://localhost:8080/admin/searchBus/${selectedBusId}`, {
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-                }
-            })
-                .then(result => {
-                    return result.json();
-                })
-                .then(data => {
-                    setBusNumber(data.busNumber)
-                    setStops(data.stops);
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-        }
-    }, [mode, selectedBusId]);
-
-    const stopIdInputRef = useRef(document.createElement('input'));
-    const saveBus = (link: string, body: {}, method: 'POST' | 'PUT') => {
-        return fetch(link, {
-            method: method,
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-            },
-            body: JSON.stringify(body)
+  mode: "Add" | "Edit";
+  onCloseHandler: () => void;
+}> = (props) => {
+  const selectedBusId = useSelector(
+    (state: RootState) => state.busesList.selectedBusId
+  );
+  const [busNumber, setBusNumber] = useState<string>("");
+  const [stops, setStops] = useState<stop[]>([]);
+  const busNumberInputRef = useRef(document.createElement("input"));
+  const { mode } = props;
+  useEffect(() => {
+    if (mode === "Edit" && selectedBusId) {
+      console.log(selectedBusId);
+      console.log(`sending request with busId ${selectedBusId}`);
+      fetch(`http://localhost:8080/admin/searchBus/${selectedBusId}`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("authToken"),
+        },
+      })
+        .then((result) => {
+          return result.json();
         })
-            .then(result => {
-                return result.json()
-            })
-            .then(data => {
-                console.log(data)
-                props.onCloseHandler();
-            })
-
-    }
-    const onSaveHandler = () => {
-        if (props.mode === 'Add') {
-            //send Post request and create new fields in DB
-            saveBus('http://localhost:8080/admin/addNewBus', {
-                busNumber: busNumberInputRef.current.value,
-                stops: stops
-            }, 'POST').catch(err => {
-                console.log(err)
-            })
-        }
-        if (props.mode === 'Edit') {
-            //send Put request and replace the existing Bus
-            saveBus('http://localhost:8080/admin/editBus', {
-                _id: selectedBusId,
-                busNumber: busNumberInputRef.current.value,
-                stops: stops
-            }, 'PUT').catch(err => {
-                console.log(err)
-            })
-        }
-    }
-    const onAddStopHandler = (event: React.FormEvent) => {
-        event.preventDefault();
-        const enteredStopId = stopIdInputRef.current.value;
-        fetch(`http://localhost:8080/admin/searchStop/${enteredStopId}`, {
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-            }
+        .then((data) => {
+          setBusNumber(data.busNumber);
+          setStops(data.stops);
         })
-            .then(result => {
-                return result.json();
-            })
-            .then(data => {
-                setStops(prevState => {
-                    const existingStop = prevState.find(stop => {
-                        return stop._id === data._id
-                    })
-                    if (existingStop) {
-                        return [...prevState]
-                    }
-                    return [...prevState, data]
-                })
-                console.log(stops);
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [mode, selectedBusId]);
 
+  const stopIdInputRef = useRef(document.createElement("input"));
+  const saveBus = (link: string, body: {}, method: "POST" | "PUT") => {
+    return fetch(link, {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("authToken"),
+      },
+      body: JSON.stringify(body),
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        console.log(data);
+        props.onCloseHandler();
+      });
+  };
+  const onSaveHandler = () => {
+    if (props.mode === "Add") {
+      saveBus(
+        "http://localhost:8080/admin/addNewBus",
+        {
+          busNumber: busNumberInputRef.current.value,
+          stops: stops,
+        },
+        "POST"
+      ).catch((err) => {
+        console.log(err);
+      });
     }
-    const onStopsClickHandler = (id: string) => {
-        setStops((prevState: stop[]) => {
-            return prevState.filter((stop) => {
-                return id !== stop._id;
-            });
-        })
+    if (props.mode === "Edit") {
+      saveBus(
+        "http://localhost:8080/admin/editBus",
+        {
+          _id: selectedBusId,
+          busNumber: busNumberInputRef.current.value,
+          stops: stops,
+        },
+        "PUT"
+      ).catch((err) => {
+        console.log(err);
+      });
     }
-    const onBusNumberChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBusNumber(event.target.value);
-    }
-    return (
-        <GrayCard cssClasses={[classes.overAllContainer]}>
-            <div className={classes.titleContainer}>
-                <h1>{props.mode} Bus</h1>
-                <NormalGradientImageButton buttonColor={''} imageLocation={close} onClick={props.onCloseHandler}/>
-            </div>
-            <GradientInput type={'text'} value={busNumber} color={'red'} placeHolder={'Enter Bus Number'}
-                           label={'Bus Number'} cssClasses={[classes.overAllInput]}
-                           onChangeHandler={onBusNumberChangeHandler} ref={busNumberInputRef}/>
-            <div className={classes.stopsContainer}>
-                <p className={classes.stopsText}>Stops</p>
-                <div className={classes.stopsSubContainer}>
-                    {stops.map((stop) => {
-                        return <p key={stop._id} className={classes.stop} onClick={() => onStopsClickHandler(stop._id)}>
-                            {stop.name}<img src={close} alt={close}/>
-                        </p>
-                    })}
-                </div>
-            </div>
-            <form className={classes.formContainer} onSubmit={onAddStopHandler}>
-                <h1 className={classes.formTitle}>Add Stops</h1>
-                <GradientInput cssClasses={[classes.formText]} type={'text'} color={'red'}
-                               placeHolder={'Enter Stop ID'} label={'Stop ID'} ref={stopIdInputRef}/>
-                <NormalGradientButton text={'Add Stop'} buttonColor={'red'}
-                                      cssClassesOnContainer={[classes.formButton]}/>
-            </form>
-            <NormalGradientButton text={'Save Bus'} buttonColor={'green'} onClick={onSaveHandler}
-                                  cssClassesOnContainer={[classes.overAllButton]}/>
-        </GrayCard>
-    )
-}
+  };
+  const onAddStopHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredStopId = stopIdInputRef.current.value;
+    fetch(`http://localhost:8080/admin/searchStop/${enteredStopId}`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("authToken"),
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        setStops((prevState) => {
+          const existingStop = prevState.find((stop) => {
+            return stop._id === data._id;
+          });
+          if (existingStop) {
+            return [...prevState];
+          }
+          return [...prevState, data];
+        });
+        console.log(stops);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onStopsClickHandler = (id: string) => {
+    setStops((prevState: stop[]) => {
+      return prevState.filter((stop) => {
+        return id !== stop._id;
+      });
+    });
+  };
+  const onBusNumberChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setBusNumber(event.target.value);
+  };
+  return (
+    <GrayCard cssClasses={[classes.overAllContainer]}>
+      <div className={classes.titleContainer}>
+        <h1>{props.mode} Bus</h1>
+        <NormalGradientImageButton
+          buttonColor={""}
+          imageLocation={close}
+          onClick={props.onCloseHandler}
+        />
+      </div>
+      <GradientInput
+        type={"text"}
+        value={busNumber}
+        color={"red"}
+        placeHolder={"Enter Bus Number"}
+        label={"Bus Number"}
+        cssClasses={[classes.overAllInput]}
+        onChangeHandler={onBusNumberChangeHandler}
+        ref={busNumberInputRef}
+      />
+      <div className={classes.stopsContainer}>
+        <p className={classes.stopsText}>Stops</p>
+        <div className={classes.stopsSubContainer}>
+          {stops.map((stop) => {
+            return (
+              <p
+                key={stop._id}
+                className={classes.stop}
+                onClick={() => onStopsClickHandler(stop._id)}
+              >
+                {stop.name}
+                <img src={close} alt={close} />
+              </p>
+            );
+          })}
+        </div>
+      </div>
+      <form className={classes.formContainer} onSubmit={onAddStopHandler}>
+        <h1 className={classes.formTitle}>Add Stops</h1>
+        <GradientInput
+          cssClasses={[classes.formText]}
+          type={"text"}
+          color={"red"}
+          placeHolder={"Enter Stop ID"}
+          label={"Stop ID"}
+          ref={stopIdInputRef}
+        />
+        <NormalGradientButton
+          text={"Add Stop"}
+          buttonColor={"red"}
+          cssClassesOnContainer={[classes.formButton]}
+        />
+      </form>
+      <NormalGradientButton
+        text={"Save Bus"}
+        buttonColor={"green"}
+        onClick={onSaveHandler}
+        cssClassesOnContainer={[classes.overAllButton]}
+      />
+    </GrayCard>
+  );
+};
 export default FormAddOrEditBus;
--- Ticketing Web Application/react/src/pages/AdminPages/StaffPage/StaffPage.tsx
+++ Ticketing Web Application/react/src/pages/AdminPages/StaffPage/StaffPage.tsx
@@ -1,80 +1,107 @@
-import React, {Fragment, useEffect} from "react";
-import classes from './StaffPage.module.css'
+import React, { Fragment, useEffect } from "react";
+import classes from "./StaffPage.module.css";
 import GrayCard from "../../../UI/GrayCard/GrayCard";
 import FormSearchStaff from "../../../components/AdminComponents/Forms/FormSearchStaff/FormSearchStaff";
 import StaffList from "../../../components/AdminComponents/Lists/StaffList/StaffList";
 import BackDrop from "../../../UI/BackDrop/BackDrop";
-import {useDispatch, useSelector} from "react-redux";
-import {staffListAction} from "../../../store/staff-list-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { staffListAction } from "../../../store/staff-list-slice";
 import FormResetPassword from "../../../components/AdminComponents/Forms/FormResetPassword/FormResetPassword";
 import ConfirmationCard from "../../../UI/ConfirmationCard/ConfirmationCard";
 import FormChangeBus from "../../../components/AdminComponents/Forms/FormChangeBus/FormChangeBus";
-import {RootState} from "../../../store";
+import { RootState } from "../../../store";
 import FormAddPeople from "../../../components/AdminComponents/Forms/FormAddPeople/FormAddPeople";
 import fetch from "node-fetch";
-import {uiAction} from "../../../store/ui-slice";
+import { uiAction } from "../../../store/ui-slice";
 
 const StaffPage: React.FC<{}> = () => {
-    const dispatch = useDispatch();
-    const isAddOpen = useSelector((state: RootState) => state.staffList.isAddOpen);
-    const isChangeBusOpen = useSelector((state: RootState) => state.staffList.isChangeBusOpen);
-    const isResetPasswordOpen = useSelector((state: RootState) => state.staffList.isResetOpen);
-    const isDeleteOpen = useSelector((state: RootState) => state.staffList.isDeleteOpen);
-    const selectedStaffId = useSelector((state: RootState) => state.staffList.selectedStaffId);
-    useEffect(() => {
-        dispatch(uiAction.closeMobileMenu());
-    },[dispatch]);
-    const onCloseHandler = () => {
-        console.log('close handler triggered')
-        dispatch(staffListAction.closeAll());
-    }
-    const onDeleteHandler = () => {
-        fetch(`http://localhost:8080/admin/deleteStaff/${selectedStaffId}`, {
-            method: 'DELETE',
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-            }
-        })
-            .then(result => {
-                return result.json();
-            })
-            .then(data => {
-                console.log(data);
-                onCloseHandler();
-                dispatch(staffListAction.clearAll());
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-    return (
-        <Fragment>
-            <BackDrop visibility={`${isAddOpen ? 'show' : 'hide'}`} onClick={onCloseHandler}>
-                <FormAddPeople type={'Staff'} onCloseHandler={onCloseHandler}/>
-            </BackDrop>
-            <BackDrop visibility={`${isChangeBusOpen ? 'show' : 'hide'}`} onClick={onCloseHandler}>
-                <FormChangeBus onCloseHandler={onCloseHandler}/>
-            </BackDrop>
-            <BackDrop visibility={`${isResetPasswordOpen ? 'show' : 'hide'}`} onClick={onCloseHandler}>
-                <FormResetPassword type={'staff'} onCloseHandler={onCloseHandler}/>
-            </BackDrop>
-            <BackDrop visibility={`${isDeleteOpen ? 'show' : 'hide'}`} onClick={onCloseHandler}>
-                <ConfirmationCard title={'Delete Staff'} message={'Are you sure do you want to delete staff?'}
-                                  leftButtonText={'No'} rightButtonText={'Yes'}
-                                  leftButtonClickHandler={onCloseHandler} rightButtonClickHandler={onDeleteHandler}
-                                  onClose={onCloseHandler}/>
-            </BackDrop>
-            <section className={classes.staffSection}>
-                <div className={classes.cardContainer}>
-                    <GrayCard cssClasses={[classes.topCard]}>
-                        <FormSearchStaff/>
-                    </GrayCard>
-                    <GrayCard cssClasses={[classes.bottomCard]}>
-                        <StaffList/>
-                    </GrayCard>
-                </div>
-            </section>
-        </Fragment>
-    )
-}
-export default StaffPage
+  const dispatch = useDispatch();
+  const isAddOpen = useSelector(
+    (state: RootState) => state.staffList.isAddOpen
+  );
+  const isChangeBusOpen = useSelector(
+    (state: RootState) => state.staffList.isChangeBusOpen
+  );
+  const isResetPasswordOpen = useSelector(
+    (state: RootState) => state.staffList.isResetOpen
+  );
+  const isDeleteOpen = useSelector(
+    (state: RootState) => state.staffList.isDeleteOpen
+  );
+  const selectedStaffId = useSelector(
+    (state: RootState) => state.staffList.selectedStaffId
+  );
+  useEffect(() => {
+    dispatch(uiAction.closeMobileMenu());
+  }, [dispatch]);
+  const onCloseHandler = () => {
+    console.log("close handler triggered");
+    dispatch(staffListAction.closeAll());
+  };
+  const onDeleteHandler = () => {
+    fetch(`http://localhost:8080/admin/deleteStaff/${selectedStaffId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("authToken"),
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        console.log(data);
+        onCloseHandler();
+        dispatch(staffListAction.clearAll());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <Fragment>
+      <BackDrop
+        visibility={`${isAddOpen ? "show" : "hide"}`}
+        onClick={onCloseHandler}
+      >
+        <FormAddPeople type={"Staff"} onCloseHandler={onCloseHandler} />
+      </BackDrop>
+      <BackDrop
+        visibility={`${isChangeBusOpen ? "show" : "hide"}`}
+        onClick={onCloseHandler}
+      >
+        <FormChangeBus onCloseHandler={onCloseHandler} />
+      </BackDrop>
+      <BackDrop
+        visibility={`${isResetPasswordOpen ? "show" : "hide"}`}
+        onClick={onCloseHandler}
+      >
+        <FormResetPassword type={"staff"} onCloseHandler={onCloseHandler} />
+      </BackDrop>
+      <BackDrop
+        visibility={`${isDeleteOpen ? "show" : "hide"}`}
+        onClick={onCloseHandler}
+      >
+        <ConfirmationCard
+          title={"Delete Staff"}
+          message={"Are you sure do you want to delete staff?"}
+          leftButtonText={"No"}
+          rightButtonText={"Yes"}
+          leftButtonClickHandler={onCloseHandler}
+          rightButtonClickHandler={onDeleteHandler}
+          onClose={onCloseHandler}
+        />
+      </BackDrop>
+      <section className={classes.staffSection}>
+        <div className={classes.cardContainer}>
+          <GrayCard cssClasses={[classes.topCard]}>
+            <FormSearchStaff />
+          </GrayCard>
+          <GrayCard cssClasses={[classes.bottomCard]}>
+            <StaffList />
+          </GrayCard>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+export default StaffPage;
